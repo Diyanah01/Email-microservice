ac25e33a4194185ef734795ec616050dbe768baa
Diyanah01 <diyanahjamal@gmail.com>
2022-03-17 14:47:51 +0800

    function user and async

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-const sgMail = require('@sendgrid/mail')
-
+const sendGridMail = require('@sendgrid/mail')
 const API_KEY = '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'
 
+sendGridMail.setApiKey(API_KEY)
 
-sgMail.setApiKey(API_KEY)
+//Notify buyer that the request has been accepted by the Seller 
+function successRequest(userEmail){
+    const body = 'Test';
+    return {
+        to : userEmail,
+        from: {
+            name :'Our company',
+            email: 'diyanahj.2020@smu.edu.sg'
+        },
+        subject: 'Request accepted',
+        text: body,
+        html: `<strong>${body}</strong>`
+    };
+}
 
-const message = {
-    to: 'diyanahjamal@gmail.com',
-    from:{
-        name:'our company',
-        email: 'diyanahj.2020@smu.edu.sg'
-    }, 
-    subject: 'TEST sendgrid',
-    text: 'Hello',
-    html: '<h1>Hello</h1>',
-};
+async function sendEmail(){
+    try{
+        await sendGridMail.send(successRequest('diyanahjamal@gmail.com')
+        ); 
+        console.log('Test email sent successfully');
+    } catch (error) {
+        console.error('Error sending test email');
+        console.error(error);
+        if (error.response){
+            console.error(error.response.body)
+        }
+    }
+}
 
-sgMail
-    .send(message)
-    .then((response) => console.log("Email sent..."))
-    .catch((error) => console.log(error.message)); 
+(async () => {
+    console.log('Sending...');
+    await sendEmail();
+})();
