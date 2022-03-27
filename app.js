951593a180fe4ef21210c9ae6a0f780b3aafc97d
KiV1 <zaki.jst@gmail.com>
2022-03-27 14:02:09 +0800

    Add qp listener

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const sendGridMail = require('@sendgrid/mail')
-const API_KEY = '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'
-
+const API_KEY = process.env.SENDGRID_API||``
 sendGridMail.setApiKey(API_KEY)
 
 function createEmail(to, subject, text){
     return{
         to,
-        from: {
-            name :'Ecommerce Shop',
-            email: 'diyanahj.2020@smu.edu.sg'
-        },
+        from:'esd.team.cs@gmail.com',
         subject,
         text,
         html: `<p>${text}</p>`
@@ -22,6 +18,30 @@ function sendEmail(to, subject, text){
     .then((response) => console.log("Email sent..."))
     .catch((error) => console.log(error.message));
 }
+var amqp = require('amqplib/callback_api');
 
+amqp.connect(process.env.AMQP || "amqp://guest:guest@localhost:5672", function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'emailToSend';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.consume(queue, function(msg) {
+            let obj = JSON.parse(msg.content);
+            sendEmail(obj.email, "Order number: "+obj.orderId, obj.body);
+            console.log(" [x] Received %s", obj.orderId);
+        }, 
+        {
+            noAck: true
+        });
+  });
+});
 //sample call that works 
-//sendEmail('diyanahjamal@gmail.com', 'test', 'test');
+//
